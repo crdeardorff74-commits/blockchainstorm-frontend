4ff8cb9aa7736af625cea823a8a7192e4a5b0b2d
crdeardorff74-commits <crdeardorff74@gmail.com>
2025-12-05 18:25:21 -0600

    Restored original Sun image

--- starfield.js
+++ starfield.js
@@ -541,7 +541,7 @@ const StarfieldSystem = (function() {
     const planets = [
         {
             name: 'Sun', level: 1, color: '#FFD700', size: 0, distance: 0,
-            imageUrl: 'https://res.cloudinary.com/dzlhmwvlx/image/upload/v1764973199/gettyimages-1406174121_gjatmr.jpg',
+            imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg',
             gravity: 28.0, tempMin: 5500, tempMax: 5500, moons: 0,
             dayLength: '25 Earth days', yearLength: 'N/A',
             funFact: 'Our star, 99.86% of solar system mass',
@@ -639,7 +639,7 @@ const StarfieldSystem = (function() {
     sunImage.crossOrigin = 'anonymous';
     sunImage.onload = () => { console.log('Sun image loaded'); };
     sunImage.onerror = () => { console.log('Sun image failed to load, using procedural'); };
-    sunImage.src = 'https://res.cloudinary.com/dzlhmwvlx/image/upload/v1764973199/gettyimages-1406174121_gjatmr.jpg';
+    sunImage.src = 'https://upload.wikimedia.org/wikipedia/commons/b/b4/The_Sun_by_the_Atmospheric_Imaging_Assembly_of_NASA%27s_Solar_Dynamics_Observatory_-_20100819.jpg';
     
     // Planet/asteroid animation state
     let planetAnimations = {};
