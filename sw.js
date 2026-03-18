a34dc8fed74080db4e3584efc4b2bfe84d28da7e
crdeardorff74-commits <crdeardorff74@gmail.com>
2026-03-18 09:31:12 -0500

    "Regae" -> "Reggae"

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // TANTЯO Service Worker
 // Bump APP_VERSION on each deploy to bust caches and notify users
-const APP_VERSION = '3.61';
+const APP_VERSION = '3.63';
 const CACHE_NAME = `tantro-v${APP_VERSION}`;
 
 // Core files to cache for offline play
