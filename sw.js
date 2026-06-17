8ef573db4e68eba4a8e7fb3e996f3cd76d10a0ec
crdeardorff74-commits <crdeardorff74@gmail.com>
2026-06-17 10:37:47 -0500

    iOS text selection block

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // TANTЯO Service Worker
 // Bump APP_VERSION on each deploy to bust caches and notify users
-const APP_VERSION = '4.20';
+const APP_VERSION = '4.21';
 const CACHE_NAME = `tantro-v${APP_VERSION}`;
 
 // Core files to cache for offline play
